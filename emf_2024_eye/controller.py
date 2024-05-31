9f94887ba792fb97e22f27bfe71618bc95059085
Warren Moore <warren@wamonite.com>
2024-05-31 12:49:47 +0100

    Rename controller bits

--- emf_2024_eye/controller.py
+++ emf_2024_eye/controller.py
@@ -23,39 +23,39 @@ class Controller:
         self.sticky = sticky
 
         # get the LPD8 device
-        self.lpd8 = LPD8()
-        self.lpd8.start()
-        self.lpd8.set_knob_limits(LPD8_PROGRAM, Knobs.ALL_KNOBS, 0, 1, is_int=False)
+        self._lpd8 = LPD8()
+        self._lpd8.start()
+        self._lpd8.set_knob_limits(LPD8_PROGRAM, Knobs.ALL_KNOBS, 0, 1, is_int=False)
         if not self.sticky:
-            self.lpd8.set_not_sticky_knob(LPD8_PROGRAM, Knobs.ALL_KNOBS)
-        self.lpd8.set_pad_mode(LPD8_PROGRAM, Pads.ALL_PADS, Pad.PUSH_MODE)
+            self._lpd8.set_not_sticky_knob(LPD8_PROGRAM, Knobs.ALL_KNOBS)
+        self._lpd8.set_pad_mode(LPD8_PROGRAM, Pads.ALL_PADS, Pad.PUSH_MODE)
 
-        self.update_flag = False
+        self._updated = False
 
         self.load_defaults()
 
         def lpd8_knob(data: tuple[int, int, float]) -> None:
             _, knob, value = data
-            self.knob_values[knob - 1] = value
-            self.update_flag = True
+            self._knobs[knob - 1] = value
+            self._updated = True
             log.debug("knob: %s = %s", knob, value)
 
         def lpd8_pad(data: tuple[int, int, float]) -> None:
             _, pad, on = data
             pad = Pads._pad_index[pad]
             on = on == 1
-            self.update_flag = True
+            self._updated = True
             # TODO do something with this
             log.debug("pad: %s = %s", pad, on)
 
-        self.lpd8.subscribe(lpd8_knob, LPD8_PROGRAM, LPD8.CTRL, Knobs.ALL_KNOBS)
-        self.lpd8.subscribe(lpd8_pad, LPD8_PROGRAM, LPD8.NOTE_ON, Pads.ALL_PADS)
-        self.lpd8.subscribe(lpd8_pad, LPD8_PROGRAM, LPD8.NOTE_OFF, Pads.ALL_PADS)
+        self._lpd8.subscribe(lpd8_knob, LPD8_PROGRAM, LPD8.CTRL, Knobs.ALL_KNOBS)
+        self._lpd8.subscribe(lpd8_pad, LPD8_PROGRAM, LPD8.NOTE_ON, Pads.ALL_PADS)
+        self._lpd8.subscribe(lpd8_pad, LPD8_PROGRAM, LPD8.NOTE_OFF, Pads.ALL_PADS)
 
     @property
     def updated(self: Self) -> bool:
-        if self.update_flag:
-            self.update_flag = False
+        if self._updated:
+            self._updated = False
             return True
 
         return False
@@ -67,36 +67,36 @@ class Controller:
         knob_index: int,
         invert: bool = False,
     ) -> float:
-        i = self.knob_values[knob_index]
+        i = self._knobs[knob_index]
         if invert:
             i = 1.0 - i
         return ((v2 - v1) * i) + v1
 
     def update(self: Self) -> None:
-        self.lpd8.pad_update()
+        self._lpd8.pad_update()
 
     def stop(self: Self) -> None:
-        self.lpd8.stop()
+        self._lpd8.stop()
 
     def load_defaults(self: Self) -> None:
-        self.knob_values = [0] * Pads.PAD_MAX
+        self._knobs = [0] * Pads.PAD_MAX
 
         try:
             with open(DEFAULTS_FILE_NAME) as file_object:
                 data = json.load(file_object)
                 if "knobs" in data:
-                    self.knob_values = data["knobs"]
+                    self._knobs = data["knobs"]
 
         except FileNotFoundError:
             pass
 
-        for idx, value in enumerate(self.knob_values):
-            self.lpd8.set_knob_value(LPD8_PROGRAM, idx + 1, value)
+        for idx, value in enumerate(self._knobs):
+            self._lpd8.set_knob_value(LPD8_PROGRAM, idx + 1, value)
 
-        self.update_flag = True
+        self._updated = True
 
     def save_defaults(self: Self) -> dict:
-        data = {"knobs": self.knob_values}
+        data = {"knobs": self._knobs}
 
         try:
             with open(DEFAULTS_FILE_NAME, "w") as file_object:
